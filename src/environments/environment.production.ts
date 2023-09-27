c3d26b29063f1ed51bf62e70b1f6c3055c06c5d1
Alec Lévesque <alec.levesque.1@ulaval.ca>
2023-09-27 00:28:16 -0400

    added firebase configuration

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,10 +1,10 @@
 export const environment = {
     delilah: {
         discord: {
-            oauth2Url: 'https://discord.com/api/oauth2/authorize?client_id=1050623612681470056&redirect_uri=https%3A%2F%2Fdelilah-ui-ba859cb2090a.herokuapp.com%2Flogin&response_type=code&scope=identify'
+            oauth2Url: 'https://discord.com/api/oauth2/authorize?client_id=1050623612681470056&redirect_uri=https%3A%2F%2Fdelilah-bot-8ecf6.web.app%2Flogin&response_type=code&scope=identify'
         },
         server: {
-            url: "https://delilah-production.herokuapp.com/"
+            url: "https://delilah-production.herokuapp.com"
         },
     }
 };
