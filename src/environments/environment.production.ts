debaf439efee368a002031181c5eafa3c84f37b8
Alec Lévesque <alec.levesque.1@ulaval.ca>
2023-10-05 03:02:51 -0400

    fixed compilation errors and domain errors

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,7 +1,7 @@
 export const environment = {
     delilah: {
         discord: {
-            oauth2Url: 'https://discord.com/api/oauth2/authorize?client_id=1050623612681470056&redirect_uri=https%3A%2F%2Fdelilah-bot-8ecf6.web.app%2Flogin&response_type=code&scope=identify'
+            oauth2Url: 'https://discord.com/api/oauth2/authorize?client_id=1050623612681470056&redirect_uri=https%3A%2F%2Fdelilahbot.com%2Flogin&response_type=code&scope=identify'
         },
         server: {
             url: "https://delilah-production.herokuapp.com"
