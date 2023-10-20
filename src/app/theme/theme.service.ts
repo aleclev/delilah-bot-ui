8805efddf31042b162431955941c610f0492a17b
Alec Lévesque <alec.levesque.1@ulaval.ca>
2023-10-20 13:42:19 -0400

    adjusted theme change for entire app, fixes datetimepicker widget styling

--- src/app/theme/theme.service.ts
+++ src/app/theme/theme.service.ts
@@ -6,16 +6,28 @@ import { Cookie } from 'ng2-cookies';
 })
 export class ThemeService {
 
-  constructor() { }
+  constructor() {
+    if (!document.documentElement.className) {
+      if (localStorage.getItem("theme")) {
+        let currentTheme = localStorage.getItem("theme")?.valueOf() as string;
+        document.documentElement.className = currentTheme;
+      } else {
+        localStorage.setItem("theme", "dark");
+        document.documentElement.className = "dark";
+      }
+    }
+  }
 
   public switchTheme(): void {
     let currentTheme: string | null = localStorage.getItem("theme");
 
     if (currentTheme === "light") {
       localStorage.setItem("theme", "dark");
+      document.documentElement.className = "dark";
     }
     else {
-      localStorage.setItem("theme", "light")
+      localStorage.setItem("theme", "light");
+      document.documentElement.className = "light";
     }
   }
 
